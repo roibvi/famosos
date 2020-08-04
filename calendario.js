80683be304c7804311031a80dac1c926d644ef74
Roberto <sirruper@gmail.com>
2020-08-04 10:26:59 +0200

    funcionando todo

--- calendario.js
+++ calendario.js
@@ -1,22 +1,35 @@
+/**Con esto traigo el servidor expres */
 var express = require("express");
 var app = express();
-
+/** Con esto hago que se comprueben los formularios. */
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+/**Con esto hago que el servidor busque todos los archivos estaticos de la página. */
 app.use(express.static("public"));
+/** Con esto le digo que debe mostrar el al acceder al servidor. */
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+/** Con esto ya cojo los datos del formulario y hago cosas con ellos.  */
 app.post("/", function(req, res) {
     /**Con esto le hago el javascript detecte el html y lo interprete, si no lo plasmaria como texto */
     res.setHeader("Content-Type", "text/html;charset=utf-8");
     /** Ahora vamos a recoger los datos del formulario. */
     var nombre = req.body.nombre;
-    var mes = req.body.mes.val();
+    var mes = Number(req.body.mes);
     var anio = Number(req.body.anio);
-
+    /**array con los nombres de los famosos */
+    var famosos = ["Mete algo idiota", "Jessica Biel", "Rihana", "NPI1", "Kristen Stewart", "Megan Fox", "Natalie Portman", "Arianna Grande", "Cara Delavigne", "NPI2", "Chastaine", "NPI3", "NPI4"];
+    /** Calculo la edad en años con una resta */
+    var edad = 2020 - anio;
+    /**Mostrar resultado final */
+    res.write("<h1> Felicidades " + nombre + "</h1>");
+    res.write("<h2> naciste el mismo mes que " + famosos[mes] + "</h2>");
+    res.write("<h3> Este año cumpliras " + edad + "</h3>");
+    res.write("<h4>años si el coronovirus lo permite.</h4>");
+    res.send();
 
 });
 
--- public/index.js
+++ public/index.js
@@ -2,43 +2,43 @@
 $("select").on("change", function() {
     var mes = $(this).val();
     switch (mes) {
-        case "-":
+        case "0":
             $("img").attr('src', 'imagenes/meses.jpg');
             break;
-        case "0":
+        case "1":
             $("img").attr('src', 'imagenes/f-enero.jpg');
             break;
-        case "1":
+        case "2":
             $("img").attr('src', 'imagenes/f-febrero.jpg');
             break;
-        case "2":
+        case "3":
             $("img").attr('src', 'imagenes/f-marzo.jpg');
             break;
-        case "3":
+        case "4":
             $("img").attr('src', 'imagenes/f-abril.jpg');
             break;
-        case "4":
+        case "5":
             $("img").attr('src', 'imagenes/f-mayo.jpg');
             break;
-        case "5":
+        case "6":
             $("img").attr('src', 'imagenes/f-junio.jpg');
             break;
-        case "6":
+        case "7":
             $("img").attr('src', 'imagenes/f-julio.jpg');
             break;
-        case "7":
+        case "8":
             $("img").attr('src', 'imagenes/f-agosto.jpg');
             break;
-        case "8":
+        case "9":
             $("img").attr('src', 'imagenes/f-septiembre.jpg');
             break;
-        case "9":
+        case "10":
             $("img").attr('src', 'imagenes/f-octubre.jpg');
             break;
-        case "10":
+        case "11":
             $("img").attr('src', 'imagenes/f-noviembre.jpg');
             break;
-        case "11":
+        case "12":
             $("img").attr('src', 'imagenes/f-diciembre.jpg');
             break;
     };
